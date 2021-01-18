ac79e6330d12bd00c5d0a42bb3ea88751daa1684
juniordugue <jrdugue@gmail.com>
2021-01-18 09:46:03 -0500

    style: few style tweaks, removed search bar from about page so its only present on the home page, and cleaned up the folder structure removing unused files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import About from "./Pages/About.js";
 import Home from "./Pages/Home.js";
 
 function App() {
-  const [pokemon, setPokemon] = useState();
+  const [pokemon, setPokemon] = useState([]);
+  const [text, setText] = useState(" ");
+  const [filteredPokemon, setFilteredPokemon] = useState([]);
+
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon?offset=0")
       .then((res) => res.json())
@@ -17,18 +20,33 @@ function App() {
       });
   });
 
+  useMemo(() => {
+    if (text.length === 0) {
+      setFilteredPokemon([]);
+      return;
+    }
+
+    setFilteredPokemon(() => pokemon.results?.filter((pokemon) => pokemon.name.includes(text)));
+  }, [pokemon.results, text]);
+
   return (
     <Router>
       <div className="p-14">
         <Link to="/">
-          <p className="text-3xl flex flex-col items-center">PoGo Community Shinies</p>
+          <p className="text-3xl flex flex-col items-center text-red-700">PoGo Community Shinies</p>
         </Link>
       </div>
+
       <Switch>
         <Route path="/about/:poke">
           <About></About>
         </Route>
-        <Route path="/">{pokemon && <Home pokemon={pokemon.results} />} </Route>
+        <Route path="/">
+          <div className="w-full flex justify-center">
+            <input type="text" onChange={($event) => setText($event.target.value)} placeholder="Type a pokemon name" className="mt-10 p-2 border-blue-400 border-2" />
+          </div>
+          {pokemon && <Home pokemon={filteredPokemon} />}{" "}
+        </Route>
       </Switch>
     </Router>
   );
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -10,16 +10,14 @@ const About = () => {
       .then((res) => res.json())
       .then((data) => {
         setPokemon(data);
-        console.log(data);
       });
   }, [poke]);
 
   return (
     <>
       {pokemon && (
-        <div className="w-3/12 m-auto bg-gray-700 mt-4 shadow-2xl flex justify-center flex-col items-center">
-          <h2 className="text-3xl text-red-900 uppercase">About</h2>
-          {poke}
+        <div className="w-3/12 m-auto bg-gray-300 mt-4 shadow-2xl flex justify-center flex-col items-center">
+          <p className="text-red-700">Pokedex entry # {poke}</p>
           <h3 className="text-2xl text-red-700 uppercase">{pokemon.name}</h3>
           <div className="flex justify-center">
             <img className="w-48" src={pokemon.sprites["front_shiny"]} alt={`${pokemon.name} front`} />
