1f37da49054f312537f50121288534678c70469a
juniordugue <jrdugue@gmail.com>
2021-01-18 09:50:32 -0500

    docs: added demo of the app

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# Demo
+
+![preview of the app](demo.gif)
+
+---
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useMemo } from "react";
-import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import About from "./Pages/About.js";
 import Home from "./Pages/Home.js";
